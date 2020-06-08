704054bf86aaa2ab6fcf97e8f7ae16cc7a8b311c
Andrew <ferulip@gmail.com>
2020-06-08 17:11:36 -0300

    initial commit

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,93 +1,120 @@
 (() => {
-    const selector = selector => /* trecho omitido */
-    const create = element => /* trecho omitido */
+  const selector = (selector) => {
+    return document.querySelector(selector);
+  };
+  const create = (element) => {
+    return document.createElement(element);
+  };
 
-    const app = selector('#app');
+  const app = selector("#app");
 
-    const Login = create('div');
-    Login.classList.add('login');
+  const Login = create("div");
+  Login.classList.add("login");
 
-    const Logo = create('img');
-    Logo.src = './assets/images/logo.svg';
-    Logo.classList.add('logo');
+  const Logo = create("img");
+  Logo.src = "./assets/images/logo.svg";
+  Logo.classList.add("logo");
 
-    const Form = create('form');
+  const Form = create("form");
 
-    Form.onsubmit = async e => {
-        e.preventDefault();
-        const [email, password] = /* trecho omitido */
+  Form.onsubmit = async (e) => {
+    e.preventDefault();
 
-        const {url} = await fakeAuthenticate(email.value, password.value);
+    const [email, password] = e.target.elements;
 
-        location.href='#users';
-        
-        const users = await getDevelopersList(url);
-        renderPageUsers(users);
-    };
+    const { url } = await fakeAuthenticate(email.value, password.value);
 
-    Form.oninput = e => {
-        const [email, password, button] = e.target.parentElement.children;
-        (!email.validity.valid || !email.value || password.value.length <= 5) 
-            ? button.setAttribute('disabled','disabled')
-            : button.removeAttribute('disabled');
-    };
+    location.href = "#users";
 
-    Form.innerHTML = /**
-    * bloco de código omitido
-    * monte o seu formulário
-    */
+    const users = await getRequest(url);
+    renderPageUsers(users);
+  };
 
-    app.appendChild(Logo);
-    Login.appendChild(Form);
+  Form.oninput = (e) => {
+    const [email, password, button] = e.target.parentElement.children;
+    !email.validity.valid || !email.value || password.value.length <= 5
+      ? button.setAttribute("disabled", "disabled")
+      : button.removeAttribute("disabled");
+  };
 
-    async function fakeAuthenticate(email, password) {
+  Form.innerHTML = `
+    <input type="text" name="email" class="input" placeholder="Entre com seu e-mail">
+    <input type="password" name="password" class="input" placeholder="Digite sua senha supersecreta" >
+    <button type="submit" disabled="disabled" >Entrar</button>
+  `;
 
-        /**
-         * bloco de código omitido
-         * aqui esperamos que você faça a requisição ao URL informado
-         */
+  app.appendChild(Logo);
+  Login.appendChild(Form);
 
-        const fakeJwtToken = `${btoa(email+password)}.${btoa(data.url)}.${(new Date()).getTime()+300000}`;
-        /* trecho omitido */
+  /**
+   * Método para pseudo autenticação, ele é responsável por requisitar uma url
+   * para a API e gerar um token que é armazenado no localStorage
+   * @param {*} email
+   * @param {*} password
+   */
+  async function fakeAuthenticate(email, password) {
+    const json = await getRequest("http://www.mocky.io/v2/5dba690e3000008c00028eb6");
 
-        return data;
+    const fakeJwtToken = `${btoa(email + password)}.${btoa(json.url)}.${
+      new Date().getTime() + 300000
+    }`;
+    localStorage.setItem("token", fakeJwtToken);
+
+    return json;
+  }
+
+  /**
+   * Método responsável por criar uma requisição
+   * a pártir da url passada no parametro
+   * @param {*} url url para requisição
+   */
+  async function getRequest(url) {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  }
+
+  /**
+   * Método responsável por renderizar a lista de itens no DOM
+   * @param {*} users lista de usuários
+   */
+  function renderPageUsers(users) {
+    app.style.justifyContent = "normal"; // Manipulando o flex para adequadção dos quadros
+
+    const flexContainer = create("div");
+    flexContainer.classList.add("flex");
+
+    let html = "";
+    users.forEach((user) => {
+      html += `
+        <a href="${user.html_url}" class="item">
+            <div class="mask">
+                <img src="${user.avatar_url}"/>
+            </div>
+            <p>${user.login}</p>
+        </a>
+      `;
+    });
+
+    Login.style.display = "none";
+    app.appendChild(flexContainer);
+    flexContainer.innerHTML = html;
+  }
+
+  /**
+   * Função assíncrona para verificação da autenticação
+   */
+  (async function () {
+    const rawToken = localStorage.getItem("token");
+    const token = rawToken ? rawToken.split(".") : null;
+    if (!token || token[2] < new Date().getTime()) {
+      localStorage.removeItem("token");
+      location.href = "#login";
+      app.appendChild(Login);
+    } else {
+      location.href = "#users";
+      const users = await getDevelopersList(atob(token[1]));
+      renderPageUsers(users);
     }
-
-    async function getDevelopersList(url) {
-        /**
-         * bloco de código omitido
-         * aqui esperamos que você faça a segunda requisição 
-         * para carregar a lista de desenvolvedores
-         */
-    }
-
-    function renderPageUsers(users) {
-        app.classList.add('logged');
-        Login.style.display = /* trecho omitido */
-
-        const Ul = create('ul');
-        Ul.classList.add('container')
-
-        /**
-         * bloco de código omitido
-         * exiba a lista de desenvolvedores
-         */
-
-        app.appendChild(Ul)
-    }
-
-    // init
-    (async function(){
-        const rawToken = /* trecho omitido */
-        const token = rawToken ? rawToken.split('.') : null
-        if (!token || token[2] < (new Date()).getTime()) {
-            localStorage.removeItem('token');
-            location.href='#login';
-            app.appendChild(Login);
-        } else {
-            location.href='#users';
-            const users = await getDevelopersList(atob(token[1]));
-            renderPageUsers(users);
-        }
-    })()
-})()
+  })();
+})();
